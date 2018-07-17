c4d5494ebb8add01db761faf3c36dcba81f4bd0e
sronga <sr899@georgetown.edu>
2018-07-17 01:25:51 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 1
-    "0"
-  elsif turn_count(board) % 2 == 0
+  if turn_count(board) % 2 == 0
     "X"
+  elsif turn_count(board) % 2 == 1
+    "O"
   end
 end
